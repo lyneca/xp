7a44badd8cd544a180b8a506df7c38541d14f463
wing2048 <lukegithub@gmail.com>
2015-10-29 14:09:47 +1100

    added basic functionality

---
+++ xp.py
@@ -0,0 +1,69 @@
+__author__ = 'Wing2048'
+from tkinter import *
+
+root = Tk()
+
+
+def get_next_level(l):
+    if l <= 10:
+        return (1.5 ** l) * 100
+    else:
+        return 57 * l * 100
+
+
+class Stat():
+    def __init__(self, r, name, x, y, l, xp):
+        self.name = name
+        self.frame = Frame(r)
+        self.frame.grid(column=x, row=y)
+        self.title = Label(self.frame, text=name)
+        self.title.grid(column=0, row=0, columnspan=3)
+        self.easy_button = Button(self.frame, text='Easy', command=self.add_easy)
+        self.hard_button = Button(self.frame, text='Hard', command=self.add_hard)
+        self.easy_button.grid(column=0, row=1)
+        self.hard_button.grid(column=1, row=1)
+        self.level = l
+        self.xp = xp
+        self.next_lvl = get_next_level(self.level)
+        self.stats = Label(self.frame, text="Lvl %s, %.1f%% to Lvl %s (%.1f / %.1f xp)" % (
+            self.level,
+            self.xp / self.next_lvl * 100,
+            self.level + 1,
+            self.xp,
+            self.next_lvl
+        ))
+        self.stats.grid(column=2, row=1)
+
+    def add_easy(self):
+        self.xp += 100
+        self.check_levels()
+
+    def add_hard(self):
+        self.xp += 200
+        self.check_levels()
+
+    def check_levels(self):
+        if self.xp >= self.next_lvl:
+            self.xp -= self.next_lvl
+            self.level += 1
+            self.next_lvl = get_next_level(self.level)
+        self.update_text()
+
+    def update_text(self):
+        self.stats.configure(text="Lvl %s, %.1f%% to Lvl %s (%s / %s xp)" % (
+            self.level,
+            self.xp / self.next_lvl * 100,
+            self.level + 1,
+            self.xp,
+            self.next_lvl
+        ))
+
+
+
+
+Stat(root, "Mathematics", 0, 0, 0, 0)
+Stat(root, "Physics", 0, 1, 0, 0)
+Stat(root, "SDD", 0, 2, 0, 0)
+Stat(root, "IPT", 0, 3, 0, 0)
+
+root.mainloop()
